33c6acc08d5862577f43fbf51a220f31dffa3cd7
Pavel Pimenov <pimenov@knoema.com>
2021-11-02 12:00:08 +0000

    version 0.1.18

--- tests/testthat/test_errors.R
+++ tests/testthat/test_errors.R
@@ -1,87 +1,39 @@
-const.client.id <- "FzOYqDgb"
-const.client.secret <-"SPrvmY8eGRcGA"
-
-test_knoema_exp <- function(expr){
-  tryCatch(
-    {
-      return(expr)
-    },
-    error = function(e) {
-      return(e$message)
-    }
-  )}
-
-
 context("test dataset id is null error")
 test_that("test dataset id is null error",{
-  data_frame = test_knoema_exp(Knoema(NULL))
-  if (data_frame != "Client error: (403) Forbidden") {
-    expect_equal(data_frame,"dataset.id should be a string. Can't be NULL")
-  } else {
-      expect_equal(data_frame,"Client error: (403) Forbidden")
-  }
+  expect_error(Knoema(NULL), "dataset.id should be a string. Can't be NULL")
 })
-
 
 context("test dataset id is double error")
 test_that("test dataset id is double error",{
-  data_frame = test_knoema_exp(Knoema(123))
-  if (data_frame != "Client error: (403) Forbidden") {
-    expect_equal(data_frame,"dataset.id should be a string. Can't be double")
-  } else {
-    expect_equal(data_frame,"Client error: (403) Forbidden")
-  }
+  expect_error(Knoema(123), "dataset.id should be a string. Can't be double")
 })
 
 context("test selection and mnemonics in one call error")
 test_that("test selection and mnemonics in one call error",{
-  data_frame = test_knoema_exp(Knoema("IMFWEO2017Oct", list(country = "912", subject = "LP;NGDP"), mnemonics = "test", client.id = const.client.id, client.secret = const.client.secret))
-  if (data_frame != "Client error: (403) Forbidden") {
-    expect_equal(data_frame,"The function does not support specifying mnemonics and selection in a single call")
-  } else {
-    expect_equal(data_frame,"Client error: (403) Forbidden")
-  }
+  expect_error(Knoema("IMFWEO2017Oct", list(country = "912", subject = "LP;NGDP"), mnemonics = "test", client.id = "FzOYqDg", client.secret="SPrvmY8eGRcGA"), 'The function does not support specifying mnemonics and selection in a single call')
 })
 
 context("test wrong dimension error")
 test_that("test wrong dimension error error",{
-  data_frame = test_knoema_exp(Knoema("IMFWEO2017Oct", list(indicator = "LP;NGDP"), client.id = const.client.id, client.secret = const.client.secret))
-  if (data_frame != "Client error: (403) Forbidden") {
-    expect_equal(data_frame,"Dimension with id or name indicator is not found")
-  } else {
-    expect_equal(data_frame,"Client error: (403) Forbidden")
-  }
+  expect_error(Knoema("IMFWEO2017Oct", list(indicator = "LP;NGDP"), client.id = "FzOYqDg", client.secret="SPrvmY8eGRcGA"), "Dimension with id or name indicator is not found")
 })
 
 context("test empty dimension selection error")
 test_that("test empty dimension selection error",{
-  data_frame = test_knoema_exp(Knoema("IMFWEO2017Oct", list(country = "", subject = "LP;NGDP"), client.id = const.client.id, client.secret = const.client.secret))
-  if (data_frame != "Client error: (403) Forbidden") {
-    expect_equal(data_frame, "Selection for dimension Country is empty")
-  } else {
-    expect_equal(data_frame,"Client error: (403) Forbidden")
-  }
+  expect_error(Knoema("IMFWEO2017Oct", list(country = "", subject = "LP;NGDP"), client.id = "FzOYqDg", client.secret="SPrvmY8eGRcGA")
+  , "Selection for dimension Country is empty")
 })
 
 context("test wrong dimension selection error")
 test_that("test wrong dimension selection error",{
-  data_frame = test_knoema_exp(Knoema("IMFWEO2017Oct", list(country=  "914;512;111", subject = "L1P;N1GDP"), client.id = const.client.id, client.secret = const.client.secret))
-  if (data_frame != "Client error: (403) Forbidden") {
-    expect_equal(data_frame, "Selection for dimension Subject is empty")
-  } else {
-    expect_equal(data_frame,"Client error: (403) Forbidden")
-  }
+  expect_error(Knoema("IMFWEO2017Oct", list(country = "914;512;111", subject = "L1P;N1GDP"), client.id = "FzOYqDg", client.secret="SPrvmY8eGRcGA")
+               ,'Selection for dimension Subject is empty')
 })
 
 context("test incorrect dataset id error")
 test_that("test incorrect dataset id error",{
-  data_frame = test_knoema_exp(Knoema("incorrect id", list(domedim = "val1;val2"), client.id = const.client.id, client.secret = const.client.secret))
-  if (data_frame != "Client error: (403) Forbidden") {
-    expect_equal(data_frame, "Requested dataset doesn't exist or you don't have access to it.")
-  } else {
-    expect_equal(data_frame,"Client error: (403) Forbidden")
-  }
-})
+  expect_error(Knoema("incorrect id", list(domedim = "val1;val2"), client.id = "FzOYqDg", client.secret="SPrvmY8eGRcGA"),"Requested dataset doesn't exist or you don't have access to it.")
+  })
 
 context("test incorrect client.id and client.secret error")
 test_that("test incorrect client.id and client.secret error",{
@@ -90,30 +42,18 @@ test_that("test incorrect client.id and client.secret error",{
 
 context("test incorrect frequencies error")
 test_that("test incorrect frequencies error",{
-  data_frame = test_knoema_exp(Knoema("IMFWEO2017Oct", list(country = "914;512;111", subject = "LP;NGDP", frequency = "A;G;R"), client.id = const.client.id, client.secret = const.client.secret))
-  if (data_frame != "Client error: (403) Forbidden") {
-    expect_equal(data_frame, "The following frequencies are not correct: G,R")
-  } else {
-    expect_equal(data_frame,"Client error: (403) Forbidden")
-  }
+  expect_error(Knoema("IMFWEO2017Oct", list(country = "914;512;111", subject = "LP;NGDP", frequency = "A;G;R"), client.id = "FzOYqDg", client.secret="SPrvmY8eGRcGA")
+               , "The following frequencies are not correct: G,R", fixed = TRUE)
 })
 
 context("test incorrect host error")
 test_that("test incorrect host error",{
-  data_frame = test_knoema_exp(Knoema("IMFWEO2017Oct", list(country = "914;512;111", subject = "LP;NGDP", frequency = "A"), host = 'knoema_incorrect.com', client.id = const.client.id, client.secret = const.client.secret))
-  if (data_frame != "Client error: (403) Forbidden") {
-    expect_equal(data_frame, "The specified host knoema_incorrect.com does not exist")
-  } else {
-    expect_equal(data_frame,"Client error: (403) Forbidden")
-  }
+  expect_error(Knoema("IMFWEO2017Oct", list(country = "914;512;111", subject = "LP;NGDP", frequency = "A"), host = 'knoema_incorrect.com', client.id = "FzOYqDg", client.secret="SPrvmY8eGRcGA")
+               , "The specified host knoema_incorrect.com does not exist", fixed = TRUE)
 })
 
 context("test unknown type error")
 test_that("test unknown type error",{
-  data_frame = test_knoema_exp( Knoema("IMFWEO2017Oct", list(country = "914;512;111", subject = "LP;NGDP", frequency = "A"), type = "incorrect_type", host = 'knoema.com', client.id = const.client.id, client.secret = const.client.secret))
-  if (data_frame != "Client error: (403) Forbidden") {
-    expect_equal(data_frame, "Unknown type incorrect_type")
-  } else {
-    expect_equal(data_frame,"Client error: (403) Forbidden")
-  }
+  expect_error(Knoema("IMFWEO2017Oct", list(country = "914;512;111", subject = "LP;NGDP", frequency = "A"), type = "incorrect_type", host = 'knoema.com', client.id = "FzOYqDg", client.secret="SPrvmY8eGRcGA")
+               , "Unknown type incorrect_type", fixed = TRUE)
 })
